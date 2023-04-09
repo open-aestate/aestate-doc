8bac0e934fbb4ba1fa51906fff7a7b40a89d6017
CACode <cacode@163.com>
2023-04-09 22:50:09 +0800

    一些配置修改

--- docs/docs/sidebar_contents.js
+++ docs/docs/sidebar_contents.js
@@ -1,27 +1,25 @@
 module.exports = [
-	{
-		title: "基本设置",
-		children: [
-			['/docs/baseConfig/1', '1.安装和先决条件'],
-			['/docs/baseConfig/2', '2.获得demo数据库'],
-			['/docs/baseConfig/3', '3.创建数据库配置'],
-			['/docs/baseConfig/4', '4.创建模板表'],
-		]
-	},
-	{
-		title: "基本使用",
-		children: [
-			['/docs/baseUsing/1', '创建一个表对象'],
-			['/docs/baseUsing/2', '查询'],
-			['/docs/baseUsing/3', '插入'],
-			['/docs/baseUsing/4', '删除'],
-			['/docs/baseUsing/5', '更新'],
-		]
-	},
-	{
-		title: "进阶orm",
-		children: [
-			['/docs/orm/article-3'],
-		]
-	}
+  {
+    title: "基本设置",
+    children: [
+      ["/docs/baseConfig/1", "1.安装和先决条件"],
+      ["/docs/baseConfig/2", "2.获得demo数据库"],
+      ["/docs/baseConfig/3", "3.创建数据库配置"],
+      ["/docs/baseConfig/4", "4.创建模板表"],
+    ],
+  },
+  {
+    title: "基本使用",
+    children: [
+      ["/docs/baseUsing/1", "创建一个表对象"],
+      ["/docs/baseUsing/2", "查询"],
+      ["/docs/baseUsing/3", "插入"],
+      ["/docs/baseUsing/4", "删除"],
+      ["/docs/baseUsing/5", "更新"],
+    ],
+  },
+  {
+    title: "进阶orm",
+    children: [["/docs/orm/1", "查询"]],
+  },
 ];
